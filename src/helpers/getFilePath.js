25d7dfa64dc7284eb352b023bc57e926acf17589
James Oliveira da Silva <jamesodas@gmail.com>
2020-01-23 00:21:05 -0300

    Fixed bug on getFilePath function

--- src/helpers/getFilePath.js
+++ src/helpers/getFilePath.js
@@ -1,3 +1,12 @@
 module.exports = function(moldFileName){
-    return `${process.cwd()}/${moldFileName}`;
+    console.log(process.cwd().match(/\\/));
+    const cwd = process.cwd();
+    const barType = cwd.match(/\\/);
+    if(barType == '\\') {
+        const moldFileNameWithBackslash = moldFileName.replace(/\//, '\\');
+        return `${cwd}\\${moldFileNameWithBackslash}`;
+    } else {
+        const moldFileNameWithBar = moldFileName.replace(/\\/, '//');
+        return `${cwd}/${moldFileNameWithBar}`;
+    }
 };
